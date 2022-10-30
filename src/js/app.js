018ac8e344bf7da5f3c991e84230609d3e8808c2
Alshager <zhandosovalshager065@gmail.com>
2022-10-30 23:22:14 +0600

    Prow Header, Banner, Partners

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,30 @@
 import '../css/app.css';
-
-import(/* webpackChunkName: "myModule", webpackPrefetch: true */ './dynamic_import').then(module => module.default());
+// core version + navigation, pagination modules:
+import Swiper from 'swiper/bundle';
+// import Swiper and modules styles
+import(/* webpackChunkName: "myModule", webpackPrefetch: true */ './dynamic_import').then((module) => module.default());
 
 console.log('Hello from app.js');
+
+const swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    '@0.00': {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    '@0.75': {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    '@1.00': {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    '@1.50': {
+      slidesPerView: 6,
+      spaceBetween: 20,
+    },
+  },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,30 @@ module.exports = {
         sans: ['Lato', ...defaultTheme.fontFamily.sans],
       },
       screens: {
-        xxl: '1410px',
+        xl: { max: '1279px' },
+        // => @media (max-width: 1279px) { ... }
+
+        lg: { max: '1023px' },
+        // => @media (max-width: 1023px) { ... }
+
+        md: { max: '767px' },
+        // => @media (max-width: 767px) { ... }
+
+        sm: { max: '639px' },
+        // => @media (max-width: 639px) { ... }
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
       },
     },
   },
   variants: {
     borderWidth: ['responsive', 'hover'],
   },
-  plugins: [require('@tailwindcss/forms', {
-    strategy: 'base', // only generate global styles
-    // strategy: 'class', // only generate classes
-  })],
+  plugins: [
+    require('@tailwindcss/forms', {
+      strategy: 'base', // only generate global styles
+      // strategy: 'class', // only generate classes
+    }),
+  ],
 };
